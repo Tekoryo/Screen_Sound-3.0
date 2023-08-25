40c5657beb3b64d3b14f65a2cf76e73328958fd9
Thiago <thiagocoelho.coelho2@gmail.com>
2023-08-24 23:32:10 -0300

    finalizacao da criacao dos menus

---
+++ ScreenSound/Menus/MenuRegistrarAlbum.cs
@@ -0,0 +1,21 @@
+﻿using ScreenSoud.Modelos;
+using ScreenSound.Menus;
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarAlbum : Menu
+{
+    public void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.Clear();
+        ExibirTituloDaOpcao("Registro de álbuns");
+        Console.Write("Digite a banda cujo álbum deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Console.Write("Agora digite o título do álbum: ");
+        string tituloAlbum = Console.ReadLine()!;
+        Banda banda = bandasRegistradas[nomeDaBanda];
+        banda.AdicionarAlbum(new Album(tituloAlbum));
+        Console.WriteLine($"O álbum {tituloAlbum} de {nomeDaBanda} foi registrado com sucesso!");
+        Thread.Sleep(4000);
+        Console.Clear();
+    }
+}
---
+++ ScreenSound/Menus/MenuSair.cs
@@ -0,0 +1,13 @@
+﻿using ScreenSoud.Modelos;
+using ScreenSound.Menus;
+namespace ScreenSound.Menus;
+
+internal class MenuSair
+{
+    public void Executar()
+    {
+        Console.WriteLine("Tchau tchau :)");
+        Thread.Sleep(2000);
+        Console.Clear();
+    }
+}
